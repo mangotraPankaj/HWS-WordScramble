93d6ca707f70131daea79743dde96647e679f91e
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-22 10:50:57 +0530

    Initial Commit

---
+++ HWS-WordScramble/HWS_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_WordScrambleApp.swift
+//  HWS-WordScramble
+//
+//  Created by Pankaj Mangotra on 22/06/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
