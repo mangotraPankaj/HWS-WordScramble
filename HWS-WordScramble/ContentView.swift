2b5c0bcf9fdb321bddeb4aadec851ddc21313088
Pankaj Mangotra <pankaj.mangotra@gmail.com>
2021-06-22 12:18:53 +0530

    Created checks for the user word : Is the word original Is the word possible from the root word Is the user entered word a real English word

--- HWS-WordScramble/ContentView.swift
+++ HWS-WordScramble/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
     
     var body: some View {
         NavigationView {
@@ -49,6 +52,33 @@ struct ContentView: View {
         rootWord = allWords.randomElement() ?? "silkWorm"
         return
     }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+        
+    }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
